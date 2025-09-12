7e7126335de38562db110aadc93e3e5870757828
M. Sami Gürpınar <sami.gurpinar@gmail.com>
2025-09-12 14:41:17 +0300

    refactor(test): Revamp framework with improved struct usage and test case handling
    - Replaced nested structs with flat arrays for simpler suite and test case management.
    - Updated equality macros for better precision and clarity.
    - Enhanced `mtRunAllTests` and `mtRunSuiteTests` with detailed logging and result reporting.
    - Improved memory management and registry cleanup for robustness.

--- include/minic.h
+++ include/minic.h
@@ -6,46 +6,47 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-#define MT_EPSILON 0.000001
+#define MT_EPSILON_FLOAT 1e-6f
+#define MT_EPSILON_DOUBLE 1e-12
 
 #define COLOR_GREEN   "\033[32m"
 #define COLOR_RED     "\033[31m"
 #define COLOR_RESET   "\033[0m"
 
-#define EXPECT_FLOAT_EQ(result, expected, func_name) \
+extern int failed;
+
+#define EXPECT_FLOAT_EQ(actual, expected) \
     do { \
-        printf(COLOR_GREEN"[ RUN      ] " COLOR_RESET"%s\n", func_name); \
-        if (fabsf((result) - (expected)) >= MT_EPSILON) { \
-            printf(COLOR_RED "[  FAILED  ] " COLOR_RESET "%s (%d ms)\n", func_name, 0); \
-            return 1;\
-        } else { \
-            printf(COLOR_GREEN "[       OK ] " COLOR_RESET "%s (%d ms)\n", func_name, 0); \
-            return 0;\
+        if (fabsf((actual) - (expected)) > MT_EPSILON_FLOAT) { \
+            ++failed; \
+            printf("%s:%d: " COLOR_RED"Failure\n" COLOR_RESET"Got %.9g, expected %.9g\n", \
+                __FILE__, __LINE__, actual, expected); \
         } \
     } while (0)
 
-#define EXPECT_DOUBLE_EQ(result, expected, func_name) \
+#define EXPECT_DOUBLE_EQ(actual, expected) \
     do { \
-        printf(COLOR_GREEN"[ RUN      ] " COLOR_RESET"%s\n", func_name); \
-        if (fabs((result) - (expected)) >= MT_EPSILON) { \
-            printf(COLOR_RED "[  FAILED  ] " COLOR_RESET "%s (%d ms)\n", func_name, 0); \
-        } else { \
-            printf(COLOR_GREEN "[       OK ] " COLOR_RESET "%s (%d ms)\n", func_name, 0); \
+        if (fabs((actual) - (expected)) > MT_EPSILON_DOUBLE) { \
+            ++failed; \
+            printf("%s:%d: " COLOR_RED"Failure\n" COLOR_RESET"Got %.17g, expected %.17g\n", \
+                __FILE__, __LINE__, actual, expected); \
         } \
     } while (0)
 
 #define TEST_FUNC_NAME(name) test_##name
-#define TEST_FUNC(name) int TEST_FUNC_NAME(name)()
+#define TEST_FUNC(name) void TEST_FUNC_NAME(name)()
 
-typedef int (* test_func)();
+typedef void (* TestFuncPtr)();
 
 typedef struct {
-    struct {
-        size_t count;
-        size_t capacity;
-        test_func* data;
-    } tests;
+    const char* name;
+    TestFuncPtr callback;
+} TestFunc;
 
+typedef struct {
+    size_t count;
+    size_t capacity;
+    TestFunc* cases;
     const char* name;
 } MTSuite;
 
@@ -57,7 +58,7 @@ void mtRunSuiteTests(const char* name);
 
 MTSuite* mtAddSuite(const char* name);
 
-void mtAddTest(MTSuite* suite, test_func funcPtr);
+void mtAddTest(MTSuite* suite, const char* name, TestFuncPtr func);
 
 void mtCleanupRegistry();
 
--- src/minic.c
+++ src/minic.c
@@ -1,81 +1,119 @@
 #include "minic.h"
 #include <string.h>
 
-typedef struct {
-    struct {
-        size_t count;
-        size_t capacity;
-        MTSuite* data;
-    } suites;
+int failed = 0;
 
+typedef struct {
+    size_t count;
+    size_t capacity;
+    MTSuite* suites;
     size_t totalTestCount;
-    size_t failed;
+    size_t failedCount;
 } MTRegistry;
 
 static MTRegistry registry;
 
 void mtInitRegistry() {
     registry.totalTestCount = 0;
-    registry.suites.count = 0;
-    registry.suites.capacity = 2;
-    registry.suites.data = malloc(registry.suites.capacity * sizeof(MTSuite));
+    registry.count = 0;
+    registry.capacity = 2;
+    registry.suites = malloc(registry.capacity * sizeof(MTSuite));
 }
 
 void mtRunAllTests() {
     printf(COLOR_GREEN"[==========] " COLOR_RESET"Running %lu tests from %lu test suites.\n",
-           registry.totalTestCount, registry.suites.count);
+           registry.totalTestCount, registry.count);
     printf(COLOR_GREEN"[----------] " COLOR_RESET"Global test environment set-up.\n");
 
 
-    for (size_t i = 0; i < registry.suites.count; i++) {
-        const MTSuite currentSuite = registry.suites.data[i];
+    for (size_t i = 0; i < registry.count; i++) {
+        const MTSuite currentSuite = registry.suites[i];
 
         printf(COLOR_GREEN"[----------] " COLOR_RESET"%lu tests from %s\n",
-            currentSuite.tests.count, currentSuite.name);
+               currentSuite.count, currentSuite.name);
 
-        for (size_t j = 0; j < currentSuite.tests.count; j++) {
-            if (currentSuite.tests.data[j]()) {
-                registry.failed++;
+        for (size_t j = 0; j < currentSuite.count; j++) {
+            const TestFunc func = currentSuite.cases[j];
+
+            printf(COLOR_GREEN"[ RUN      ] " COLOR_RESET"%s.%s\n", currentSuite.name, func.name);
+
+            func.callback();
+
+            if (failed) {
+                registry.failedCount++;
+
+                printf(COLOR_RED "[  FAILED  ] " COLOR_RESET "%s.%s (%d ms)\n", currentSuite.name, func.name, 0);
+                failed = 0;
+            } else {
+                printf(COLOR_GREEN "[       OK ] " COLOR_RESET "%s.%s (%d ms)\n", currentSuite.name, func.name, 0);
             }
         }
 
         printf(COLOR_GREEN"[----------] " COLOR_RESET"%lu tests from %s (%d ms total)\n\n",
-            currentSuite.tests.count, currentSuite.name, 0);
+               currentSuite.count, currentSuite.name, 0);
     }
 
     printf(COLOR_GREEN"[----------] " COLOR_RESET"Global test environment tear-down.\n");
     printf(COLOR_GREEN"[==========] " COLOR_RESET"%lu tests from %lu test suites ran. (%d ms total)\n",
-           registry.totalTestCount, registry.suites.count, 0);
+           registry.totalTestCount, registry.count, 0);
 
-    if (registry.failed == 0) {
+    if (registry.failedCount == 0) {
         printf(COLOR_GREEN "[  PASSED  ] " COLOR_RESET "%lu tests.\n", registry.totalTestCount);
     } else {
-        printf(COLOR_GREEN "[  PASSED  ] " COLOR_RESET "%lu tests.\n", registry.totalTestCount - registry.failed);
-        printf(COLOR_RED   "[  FAILED  ] " COLOR_RESET "%lu tests.\n", registry.failed);
+        printf(COLOR_GREEN "[  PASSED  ] " COLOR_RESET "%lu tests.\n", registry.totalTestCount - registry.failedCount);
+        printf(COLOR_RED "[  FAILED  ] " COLOR_RESET "%lu tests.\n", registry.failedCount);
     }
 }
 
 void mtRunSuiteTests(const char* name) {
-    for (size_t i = 0; i < registry.suites.count; i++) {
-        const MTSuite currentSuite = registry.suites.data[i];
+    for (size_t i = 0; i < registry.count; i++) {
+        const MTSuite currentSuite = registry.suites[i];
 
-        if (strcmp(currentSuite.name, name) == 0)
+        if (strcmp(currentSuite.name, name) != 0)
             continue;
+        printf(COLOR_GREEN"[==========] " COLOR_RESET"Running %lu tests from 1 test suites.\n", currentSuite.count);
+        printf(COLOR_GREEN"[----------] " COLOR_RESET"Global test environment set-up.\n");
+        printf(COLOR_GREEN"[----------] " COLOR_RESET"%lu tests from %s\n",
+              currentSuite.count, currentSuite.name);
 
-        for (size_t j = 0; j < currentSuite.tests.count; j++) {
-            currentSuite.tests.data[j]();
+        for (size_t j = 0; j < currentSuite.count; j++) {
+            const TestFunc func = currentSuite.cases[j];
+
+            printf(COLOR_GREEN"[ RUN      ] " COLOR_RESET"%s.%s\n", currentSuite.name, func.name);
+
+            func.callback();
+
+            if (failed) {
+                registry.failedCount++;
+
+                printf(COLOR_RED "[  FAILED  ] " COLOR_RESET "%s.%s (%d ms)\n", currentSuite.name, func.name, 0);
+                failed = 0;
+            } else {
+                printf(COLOR_GREEN "[       OK ] " COLOR_RESET "%s.%s (%d ms)\n", currentSuite.name, func.name, 0);
+            }
         }
+        printf(COLOR_GREEN"[----------] " COLOR_RESET"%lu tests from %s (%d ms total)\n\n",
+              currentSuite.count, currentSuite.name, 0);
+        printf(COLOR_GREEN"[----------] " COLOR_RESET"Global test environment tear-down.\n");
+        printf(COLOR_GREEN"[==========] " COLOR_RESET"%lu tests from 1 test suites ran. (%d ms total)\n",
+               currentSuite.count, 0);
 
+        if (registry.failedCount == 0) {
+            printf(COLOR_GREEN "[  PASSED  ] " COLOR_RESET "%lu tests.\n", currentSuite.count);
+        } else {
+            printf(COLOR_GREEN "[  PASSED  ] " COLOR_RESET "%lu tests.\n", currentSuite.count - registry.failedCount);
+            printf(COLOR_RED "[  FAILED  ] " COLOR_RESET "%lu tests.\n", registry.failedCount);
+        }
         break;
     }
 }
 
 MTSuite* mtAddSuite(const char* name) {
-    if (registry.suites.count == registry.suites.capacity) {
-        registry.suites.capacity *= 2;
-        registry.suites.data = realloc(registry.suites.data, registry.suites.capacity * sizeof(MTSuite));
+    if (registry.count == registry.capacity) {
+        registry.capacity *= 2;
+        registry.suites = realloc(registry.suites, registry.capacity * sizeof(MTSuite));
 
-        if (!registry.suites.data) {
+        if (!registry.suites) {
             fprintf(stderr, "realloc failed");
             return NULL;
         }
@@ -83,37 +121,39 @@ MTSuite* mtAddSuite(const char* name) {
 
     MTSuite suite;
     suite.name = name;
-    suite.tests.count = 0;
-    suite.tests.capacity = 2;
+    suite.count = 0;
+    suite.capacity = 2;
+    suite.cases = malloc(suite.capacity * sizeof(TestFunc));
 
-    suite.tests.data = malloc(suite.tests.capacity * sizeof(test_func));
-    if (!suite.tests.data) {
+    if (!suite.cases) {
         fprintf(stderr, "malloc failed");
         return NULL;
     }
 
-    const size_t idx = registry.suites.count++;
-    registry.suites.data[idx] = suite;
+    const size_t idx = registry.count++;
+    registry.suites[idx] = suite;
 
-    return &registry.suites.data[idx];
+    return &registry.suites[idx];
 }
 
-void mtAddTest(MTSuite* suite, const test_func funcPtr) {
-    if (suite->tests.count == suite->tests.capacity) {
-        suite->tests.capacity *= 2;
-        suite->tests.data = realloc(suite->tests.data, suite->tests.capacity * sizeof(test_func));
+void mtAddTest(MTSuite* suite, const char* name, const TestFuncPtr func) {
+    if (suite->count == suite->capacity) {
+        suite->capacity *= 2;
+        suite->cases = realloc(suite->cases, suite->capacity * sizeof(TestFunc));
 
-        if (!suite->tests.data) {
+        if (!suite->cases) {
             fprintf(stderr, "realloc failed");
             return;
         }
     }
 
-    suite->tests.data[suite->tests.count++] = funcPtr;
+    suite->cases[suite->count++] = (TestFunc){name, func};
     registry.totalTestCount++;
 }
 
 void mtCleanupRegistry() {
-    free(registry.suites.data->tests.data);
-    free(registry.suites.data);
+    for (size_t i = 0; i < registry.count; i++) {
+        free(registry.suites[i].cases);
+    }
+    free(registry.suites);
 }
