364262ba8165c56fc5a009f25e83cce00c75aa0e
M. Sami Gürpınar <sami.gurpinar@gmail.com>
2025-09-12 14:03:43 +0300

    feat(test): Add separate tests for float and double addition functions
    - Renamed `add` to `addf` for float addition and introduced `addd` for double addition.
    - Added `addf` and `addd` tests to `Math` suite using appropriate equality checks.
    - Updated `main` to register and run the new test suite.

--- test/example.c
+++ test/example.c
@@ -1,17 +1,28 @@
 #include "minic.h"
 
-float add(const float x, const float y) {
+float addf(const float x, const float y) {
     return x + y;
 }
 
-TEST_FUNC(add) {
-    EXPECT_FLOAT_EQ(add(3.0f, 5.0f), 8.0f, "AddTest.add");
+double addd(const double x, const double y) {
+    return x + y;
+}
+
+TEST_FUNC(addf) {
+    EXPECT_FLOAT_EQ(addf(3.0f, 5.0f), 8.0f);
+}
+
+TEST_FUNC(addd) {
+    EXPECT_DOUBLE_EQ(addd(3.0, 5.0), 8.0);
 }
 
 int main() {
     mtInitRegistry();
-    MTSuite* suite = mtAddSuite("AddTest");
-    mtAddTest(suite, TEST_FUNC_NAME(add));
+    MTSuite* math = mtAddSuite("Math");
+
+    mtAddTest(math, "addf", TEST_FUNC_NAME(addf));
+    mtAddTest(math, "addd", TEST_FUNC_NAME(addd));
+
     mtRunAllTests();
     mtCleanupRegistry();
     return 0;
